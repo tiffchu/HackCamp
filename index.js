4affa1ed8392e94e7fcd5bd581f12ccfcdfa601f
Keigo Lee <keigo-l@Live.com>
2020-12-06 11:22:16 -0800

    secured bot token

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+require('dotenv-flow').config();
+
 const Discord = require('discord.js')
 const bot = new Discord.Client();
+const prefix = "!"
 
-bot.login('Nzg1MjE3NDY4NDkwMzE3ODI0.X80owQ._-j46parqbEUcTr3lxH6z-ZXwMI')
+bot.login(process.env.TOKEN);
 
 bot.on('ready', () => {
     console.log('Logged in as ' + bot.user.tag);
 });
 
 bot.on('message', msg => {
-    if(msg.content === 'ping') {
+    if (msg.content.startsWith(prefix + 'ping')) {
         msg.reply('pong');
+    } else if (msg.content.startsWith(prefix + 'hello')) {
+        var content = msg.content.trim().split(' ');
+        msg.reply('Hello ' + content[1]);
     }
 })
