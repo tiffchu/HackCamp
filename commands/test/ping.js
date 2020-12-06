5dae5fb63f7c508c7a872dc7868ba2670da8efc9
Keigo Lee <keigo-l@Live.com>
2020-12-06 11:42:31 -0800

    update

---
+++ commands/test/ping.js
@@ -0,0 +1,24 @@
+const { Command } = require('discord.js-commando');
+
+// Command export
+module.exports = class PingCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'ping',
+            group: 'test',
+            memberName: 'ping and pong',
+            description: 'Will reply pong',
+						args: [
+								{
+										key: 'name',
+										prompt: 'name to say hi',
+										type: 'string',
+								}
+						]
+        });
+    }
+
+run(message, {name}) {
+	// Command code here!
+}
+};
--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 require('dotenv-flow').config();
 
 const Discord = require('discord.js')
-const { CommandoClient } = require('discord.js-commando');
-
-const bot = new CommandoClient({
-	commandPrefix: prefix,
-});
+//const { CommandoClient } = require('discord.js-commando');
 const prefix = "!"
+const bot = new Discord.Client();
+// const bot = new CommandoClient({
+// 	commandPrefix: prefix,
+// });
 
-bot.registry
-    .registerDefaultTypes()
-    .registerGroup('test', 'test group')
-    .registerDefaultGroups()
-    .registerDefaultCommands()
-    .registerCommandsIn(__dirname + '/commands');
+// bot.registry
+//     .registerDefaultTypes()
+//     .registerGroup('test', 'test group')
+//     .registerDefaultGroups()
+//     .registerDefaultCommands()
+//     .registerCommandsIn(__dirname + '/commands');
 
 bot.login(process.env.TOKEN);
 
@@ -31,7 +31,7 @@ bot.on('message', msg => {
 })
 
 bot.on('message', msg => {
-    if(msg.content. startWith(prefix + 'bye')) {
+    if(msg.content.startsWith(prefix + 'bye')) {
         msg.reply('bye!');
     }
 })
